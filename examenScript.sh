eac5efdf397483443a4503e3cc2a9cbe0705bbcf
smeggie <maartenmiers@gmail.com>
2015-05-29 11:26:22 +0200

    made helpfct

--- examenScript.sh
+++ examenScript.sh
@@ -6,50 +6,32 @@ INVALID_IP=2
 
 IP_PREFIX="192.168.0."
 
-function help() {
-	clear
-    echo "NAME"
-    echo "  Pinger - A tool used to ping a range of IP's"
-    echo ""
-    echo "SYNOPSIS"
-    echo "  $0 [OPTION]... [bla]"
-    echo ""
-    echo "DESCRIPTION"
-    echo "  Pinger was made to verify the status of 1 or more IP adresses in"
-    echo "  a local area network."
-    echo ""
-    echo "OPTIONS"
-    echo "  -h,"
-    echo ""
-    echo "AUTHOR"
-    echo "  Witten by Verscheure Bengt and Miers Maarten."
-	exit
-}
-
 # Ping-function
 # will ping given ip and print a message depending
 # on the ping-result
 function pingFunction() {
-	if [ "$1" -eq "$1" ] &> /dev/null
-	then
-		ping -c 1 $IP_PREFIX$1 &> /dev/null
+	ping -c 1 $IP_PREFIX$1 &> /dev/null
 
-		# the exit-code of the ping-command will be stored inside '$?'
-		# in case it's 0, the ping was successful. 
-		# for more info see: http://www.manpagez.com/man/8/ping/
-		if [[ "$?" == 0 ]]; 
-		then
-			echo "$HOST_REACHABLE"
-		else 
-			echo "$HOST_UNREACHABLE"
-		fi
-	else
-		echo "$INVALID_IP"
+	# the exit-code of the ping-command will be stored inside '$?'
+	# in case it's 0, the ping was successful. 
+	# for more info see: http://www.manpagez.com/man/8/ping/
+	if [[ "$?" == 0 ]]; 
+	then
+		echo "$HOST_REACHABLE"
+	else 
+		echo "$HOST_UNREACHABLE"
 	fi
 }
 
 function addIPToList() {
-	IP_LIST="$IP_LIST $1"
+	if [ "$1" -eq "$1" ] &> /dev/null
+	then
+            IP_LIST="$IP_LIST $1"
+	
+	else
+		# if the exit-code is 2, then the IP adress will be marked invalid.
+		echo "$IP_PREFIX$1 is an invalid IP address"
+	fi
 }
 
 # main loop/case stuff
@@ -76,3 +58,44 @@ for ip in $IP_LIST
 do
 	pingFunction $ip
 done
+
+#PLACE ON TOP PLIZ
+function help() {
+    clear
+    echo "NAME"
+    echo "  Pinger - A tool used to ping a range of IP's."
+    echo ""
+    echo "SYNOPSIS"
+    echo "  $0 [OPTION] [IP_1] [IP_2] ... [IP_N]"
+    echo ""
+    echo "DESCRIPTION"
+    echo "  Pinger was made to verify the status of 1 or more IP addresses in"
+    echo "  a local area network."
+    echo "  e.g: $0 87 97 132"
+    echo ""
+    echo "OPTIONS"
+    echo "  -h, -help"
+    echo "      help will show the user basic information about the script"
+    echo ""
+    echo "  -t"
+    echo "      this adds 200 to each number, e.g: -t 17 will test IP 217"
+    echo ""
+    echo "  --up"
+    echo "      shows you all the IP's that are up and running"
+    echo ""
+    echo "  --sum"
+    echo "      shows you a summary table of all the failed and successful pings"
+    echo ""
+    echo "  --sort"
+    echo "      pings unique IP's in descending order"
+    echo ""
+    echo "  -sn [subnet], -sn[subnet]"
+    echo '      "-sn 128" will test the subnetwork: 128 and not 192.168."129".0/24 '
+    echo ""
+    echo "  -mac"
+    echo "      every successful ping will receive the corresponding MAC-address"
+    echo ""
+    echo "AUTHOR"
+    echo "  Witten by Verscheure Bengt and Miers Maarten."
+    exit
+}
